93a516b9747fc25ae355fce906fec28586f2b22f
Danny <93397389+Dcastro99@users.noreply.github.com>
2022-03-17 13:21:57 -0700

    search bar works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import axios from 'axios';
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import './App.css';
 import CityForm from './CityForm';
+import Main from './Main';
+import Header from './Header';
+import Footer from './Footer';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,16 +17,22 @@ class App extends React.Component {
 
   getLocation = async () => {
     const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.handleChange}&format=json`;
+    const locationResponse = await axios.get(url);
+    this.setState({ locationObj: locationResponse.data[0] });
   };
 
-  handleChange = (event) => {
+  nameSearch = (event) => {
     return event.taget.value;
   };
   render() {
     return (
-      <div className="App">
-        <CityForm handleChange={this.handleChange} />
-      </div>
+      <Container className="App">
+        <Header />
+        <CityForm nameSearch={this.nameSearch} getLocation={this.getLocation} />
+
+        <Main />
+        <Footer />
+      </Container>
     );
   }
 }
--- src/CityForm.js
+++ src/CityForm.js
@@ -1,21 +1,22 @@
-import { Button } from 'bootstrap';
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Form from 'React-bootstrap/Form';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class CityForm extends React.Component {
   render() {
     return (
       <>
         <Container id="cityform">
+          <p>Hello</p>
           <Form>
             <Form.Label>
-              <Form.Select
+              <Form.Control
                 id="select"
-                onChange={(event) => this.props.handleChange(event)}
+                onChange={(event) => this.props.nameSearch(event)}
                 placeholder="Find a City!"
-              ></Form.Select>
-              <Button>Explore!</Button>
+              />
+              <Button onClick={() => this.props.getLoaction}>Explore!</Button>
             </Form.Label>
           </Form>
         </Container>
